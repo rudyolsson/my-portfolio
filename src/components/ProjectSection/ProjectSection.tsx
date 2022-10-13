e62e7c4499de732c601946eb732a8fd1fb95cb87
Rudy Olsson <rudyolsson@gmail.com>
2022-10-13 10:12:29 -0400

    Remove react-lazyload

--- src/components/ProjectSection/ProjectSection.tsx
+++ src/components/ProjectSection/ProjectSection.tsx
@@ -1,6 +1,5 @@
 import { HTMLAttributes } from 'react';
 import { FadeIn } from 'components/FadeIn';
-import LazyLoad from 'react-lazyload';
 import { Project } from 'types/interfaces';
 import * as S from './styles';
 
@@ -12,9 +11,7 @@ export const ProjectSection = ({ project }: Props) => {
   const { imgURL, bgColor, title, description, skills, siteURL } = project;
   return (
     <S.Wrapper $bgColor={bgColor}>
-      <LazyLoad height={350} offset={200}>
         <FadeIn component={<S.ScreenShot src={imgURL} alt={`An image of the app ${title}`} />} />
-      </LazyLoad>
       <FadeIn component={<S.Title>{title}</S.Title>} />
       <FadeIn component={<S.Paragraph>{description}</S.Paragraph>} />
       <FadeIn component={<S.Tech>Skills: {skills}</S.Tech>} delay={100} />
